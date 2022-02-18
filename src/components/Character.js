1cbc48fedd43c901f7fd0ebe012d02500d760e7e
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-02-18 13:56:06 -0700

    Set up Characters Component_data & function

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = props => {
+    return(
+        <div>
+            {props.bio.name}
+            <button onClick={() => props.openBio(props.bio.name)}>
+                See Bio
+                </button>
+        </div>
+    )
+}
+
+export default Character;
